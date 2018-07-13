c21010b07ab64187e92bc1b2a5fda70c16832e37
naoppy <e1710@s.akashi.ac.jp>
2018-07-13 12:11:32 +0900

    クラスを作成

---
+++ src/Person.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+
+/**
+ * プレイヤーを表すクラス
+ */
+public class Person {
+    private Point nowPoint;
+
+    private boolean isRedTeamMember;
+
+    private boolean isFinishNextSelect = false;
+
+    private Point applyPoint;
+
+    private Selection selection;
+
+    public Person(boolean isRedTeamMember) {
+        this.isRedTeamMember = isRedTeamMember;
+    }
+
+    public boolean isRedTeamMember() {
+        return isRedTeamMember;
+    }
+
+    public boolean isFinishNextSelect() {
+        return isFinishNextSelect;
+    }
+
+    public boolean select(Selection selection, Point applyPoint) {
+        switch (selection) {
+            case WAIT:
+                this.isFinishNextSelect = true;
+                this.selection = selection;
+                this.applyPoint = nowPoint;
+                return true;
+            case PUT:
+                break;
+            case REMOVE:
+                break;
+        }
+    }
+}
---
+++ src/Selection.java
@@ -0,0 +1,5 @@
+public enum Selection {
+    WAIT,
+    REMOVE,
+    PUT,
+}
