c2499086c0a64cfaa44ca2245b240610fbc1905c
Naoki Otani <e1710@s.akashi.ac.jp>
2018-09-04 15:37:23 +0900

    リファクタリング敢行　GUI機能を移譲

--- src/procon29/akashi/gui/Viewer.java
+++ src/procon29/akashi/gui/Viewer.java
@@ -1,14 +1,24 @@
 package procon29.akashi.gui;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import procon29.akashi.GameBoard;
+
+import java.awt.*;
+import java.io.IOException;
 
 public class Viewer {
     /**
      * GUIの基底
      */
     private BorderPane root;
+    /**
+     * GUIのコントローラークラス
+     */
+    private Controller controller;
     /**
      * Viewerが視覚化する対象
      */
@@ -21,6 +31,62 @@ public class Viewer {
      */
     public Viewer(GameBoard gameBoard) {
         this.gameBoard = gameBoard;
+        try {
+            //load FXML and load controller
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/root2.fxml"));
+            root = loader.load();
+            controller = loader.getController();
+
+            for (int y = 0; y < gameBoard.maker.getHeight(); y++) {
+                for (int x = 0; x < gameBoard.maker.getWidth(); x++) {
+                    //ImageViewをクリックすると追加できるように
+                    ImageView imageView = new ImageView("NoneTile.png");
+                    int yy = y, xx = x;
+                    imageView.setOnMouseClicked(event -> {
+                        Point p = new Point(xx, yy);
+                        if (!gameBoard.enemyPlayerSet.remove(p) && gameBoard.enemyPlayerSet.size() < 2) {
+                            gameBoard.enemyPlayerSet.add(p);
+                        }
+                        this.firstViewUpdate();
+                    });
+                    controller.grid.add(imageView, x, y);
+
+                    //ボタンを押したら決定できるように
+                    controller.solveBotton.setOnMouseClicked(event -> gameBoard.decideEnemyPlayerPlace());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.firstViewUpdate();
+    }
+
+    /**
+     * 敵プレイヤーの位置より初期のタイル所有マップを作る
+     */
+    private void firstViewUpdate() {
+        int w = gameBoard.maker.getWidth();
+
+        for (int y = 0; y < gameBoard.maker.getHeight(); y++) {
+            for (int x = 0; x < gameBoard.maker.getWidth(); x++) {
+                ImageView imageView = (ImageView) controller.grid.getChildren().get(w * y + x);
+                imageView.setImage(new Image("NoneTile.png"));
+            }
+        }
+        ImageView imageView = (ImageView) controller.grid.getChildren().get(w * fp1.getNowPoint().y + fp1.getNowPoint().x);
+        imageView.setImage(new Image("FriendPlayer1.png"));
+        imageView = (ImageView) controller.grid.getChildren().get(w * fp2.getNowPoint().y + fp2.getNowPoint().x);
+        imageView.setImage(new Image("FriendPlayer2.png"));
+        Point[] enemys = gameBoard.enemyPlayerSet.toArray(new Point[2]);
+        switch (gameBoard.enemyPlayerSet.size()) {
+            case 2:
+                imageView = (ImageView) controller.grid.getChildren().get(w * enemys[1].y + enemys[1].x);
+                imageView.setImage(new Image("EnemyPlayer2.png"));
+            case 1:
+                imageView = (ImageView) controller.grid.getChildren().get(w * enemys[0].y + enemys[0].x);
+                imageView.setImage(new Image("EnemyPlayer1.png"));
+                break;
+        }
     }
 
     /**
