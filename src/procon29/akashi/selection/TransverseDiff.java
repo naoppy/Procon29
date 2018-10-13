a7a1bb01a863c14b4cb1c866a668485f8ad63609
Naoki Otani <e1710@s.akashi.ac.jp>
2018-10-13 20:59:59 +0900

    X座標、Y座標の差を表すクラスをそれぞれ作成

---
+++ src/procon29/akashi/selection/TransverseDiff.java
@@ -0,0 +1,19 @@
+package procon29.akashi.selection;
+
+/**
+ * 横方向の差を表すクラス
+ */
+public enum TransverseDiff {
+    /**
+     * x方向に+1
+     */
+    Right,
+    /**
+     * x方向に-1
+     */
+    Left,
+    /**
+     * x方向は変わらない
+     */
+    None,
+}
