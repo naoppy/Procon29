2ec62cd5d6c57260a5e586b5b749d80a5299732c
naoppy <e1710@s.akashi.ac.jp>
2018-10-18 12:39:45 +0900

    クラスRandomSelectを作成

---
+++ src/procon29/akashi/solver/RandomSelect.java
@@ -0,0 +1,13 @@
+package procon29.akashi.solver;
+
+import procon29.akashi.owners.Owner;
+import procon29.akashi.players.Player;
+
+/**
+ * ランダムに打つ手を決める
+ */
+public class RandomSelect implements Solver {
+    public void solve(int[][] scores, Owner[][] owners, Player[] players) {
+
+    }
+}
