8b301cdc3e9c3dd5ea527dd4c9299b8f09e5a3e6
naoppy <e1710@s.akashi.ac.jp>
2018-10-18 14:42:45 +0900

    AllScoreCalcにメソッドを追加、内部は未実装

---
+++ src/procon29/akashi/AllScoreCalc.java
@@ -0,0 +1,18 @@
+package procon29.akashi;
+
+import procon29.akashi.owners.Owner;
+
+import java.awt.*;
+import java.util.stream.IntStream;
+
+public class AllScoreCalc {
+    private AllScoreCalc() {
+    }
+
+    public static int calc(GameBoard board, Owner targetTeam) {
+        int h = board.maker.getHeight(), w = board.maker.getWidth();
+
+        //所有しているタイルの点数を加算
+
+    }
+}
