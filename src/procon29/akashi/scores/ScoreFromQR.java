a8066fbbb75699a995cfe2afc20597250fbb839e
Naoki Otani <e1710@s.akashi.ac.jp>
2018-10-20 17:26:14 +0900

    ScoreFromQRに回転処理を実装

--- src/procon29/akashi/scores/ScoreFromQR.java
+++ src/procon29/akashi/scores/ScoreFromQR.java
@@ -13,6 +13,12 @@ public class ScoreFromQR extends ScoreMaker {
      */
     private String QRData;
 
+    private int side;
+    /**
+     * QRコードで読み取った高さと幅
+     */
+    private int QRHeight = 0, QRWidth = 0;
+
     /**
      * QR文字列を受け取ってScoreFromQRクラスを作成します
      *
@@ -37,26 +43,40 @@ public class ScoreFromQR extends ScoreMaker {
         //System.err.println(ReFormattedQRData);
         Scanner sc = new Scanner(ReFormattedQRData);
 
-        int h = sc.nextInt();
-        int w = sc.nextInt();
-        setHeight(h);
-        setWidth(w);
+        inputSide();
+        inputTurnNumber();
 
-        int[][] scores = new int[h][w];
-        for (int y = 0; y < h; y++) {
-            for (int x = 0; x < w; x++) {
+        //高さと幅の読み取り
+        QRHeight = sc.nextInt();
+        QRWidth = sc.nextInt();
+        //スコアの読み取り
+        int[][] scores = new int[QRHeight][QRWidth];
+        for (int y = 0; y < QRHeight; y++) {
+            for (int x = 0; x < QRWidth; x++) {
                 scores[y][x] = sc.nextInt();
             }
         }
-
+        //エージェント初期座標の読み取り
         int fp1y = sc.nextInt() - 1;
         fp1 = new Point(sc.nextInt() - 1, fp1y);
         int fp2y = sc.nextInt() - 1;
         fp2 = new Point(sc.nextInt() - 1, fp2y);
 
-        setMap(scores);
+        //回転
+        if (side == 1) {
+            fp1 = rotate90(fp1);
+            fp2 = rotate90(fp2);
+            scores = rotate90(scores);
+        } else if (side == 2) {
+            fp1 = rotate270(fp1);
+            fp2 = rotate270(fp2);
+            scores = rotate270(scores);
+        }
+        //盤面を90度/270度回転すると、高さと幅は入れ変わる
+        setHeight(QRWidth);
+        setWidth(QRHeight);
 
-        inputTurnNumber();
+        setMap(scores);
     }
 
     /**
@@ -67,6 +87,73 @@ public class ScoreFromQR extends ScoreMaker {
         System.out.println("ターン数を入力してください。");
 
         setTurnNumber(sc.nextInt());
+    }
+
+    /**
+     * 自分のチームが右か左か入力する
+     */
+    private void inputSide() {
+        Scanner sc = new Scanner(System.in);
+        int i;
+        do {
+            System.out.println("舞台側を上として、チームの位置は右か左か入力してください。");
+            System.out.println("1：右    2：左");
+            i = sc.nextInt();
+        } while (!(i == 1 || i == 2));
+
+        this.side = i;
+    }
+
+    /**
+     * 座標を90度回転する
+     *
+     * @param point 回転対象
+     * @return 新しく作られる回転後の座標
+     */
+    private Point rotate90(Point point) {
+        return new Point(QRHeight - 1 + point.y, point.x);
+    }
+
+    /**
+     * 90度回転させた二次元配列を作成する
+     *
+     * @param arr 回転対象
+     * @return 新しく作られる回転後の二次元配列
+     */
+    private int[][] rotate90(int[][] arr) {
+        int[][] newArr = new int[QRWidth][QRHeight];
+        for (int y = 0; y < QRHeight; y++) {
+            for (int x = 0; x < QRWidth; x++) {
+                newArr[x][QRHeight - 1 - y] = arr[y][x];
+            }
+        }
+        return newArr;
+    }
+
+    /**
+     * 座標を270度回転する
+     *
+     * @param point 回転対象
+     * @return 新しく作られる回転後の座標
+     */
+    private Point rotate270(Point point) {
+        return new Point(QRHeight - 1 + point.y, point.x);
+    }
+
+    /**
+     * 270度回転させた二次元配列を作成する
+     *
+     * @param arr 回転対象
+     * @return 新しく作られる回転後の二次元配列
+     */
+    private int[][] rotate270(int[][] arr) {
+        int[][] newArr = new int[QRWidth][QRHeight];
+        for (int y = 0; y < QRHeight; y++) {
+            for (int x = 0; x < QRWidth; x++) {
+                newArr[QRWidth - 1 - x][y] = arr[y][x];
+            }
+        }
+        return newArr;
     }
 
     /**
