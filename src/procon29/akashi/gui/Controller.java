3156dffc652435380f83afa630cd4504f6d314ad
Naoki Otani <e1710@s.akashi.ac.jp>
2018-10-20 18:12:02 +0900

    fxmlを修正

--- src/procon29/akashi/gui/Controller.java
+++ src/procon29/akashi/gui/Controller.java
@@ -2,26 +2,20 @@ package procon29.akashi.gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
 public class Controller {
 
     @FXML
-    public Label cnt1;
+    public Text cnt1;
 
     @FXML
-    public Label cnt2;
+    public Text cnt2;
 
     @FXML
-    public Label winOrLose;
-
-    @FXML
-    public Label leftSide;
-
-    @FXML
-    public Label rightSide;
+    public Text winOrLose;
 
     @FXML
     public ImageView fp1;
